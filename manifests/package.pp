8d2c71c30846c07e654be493594e64c34fba7110
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-13 01:39:02 +0530

    safadsf

--- manifests/package.pp
+++ manifests/package.pp
@@ -5,4 +5,8 @@ class nginx::package {
   package {['epel-release', 'nginx']:
     ensure => installed,
   }
+
+  package {['apt-utils', 'autoconf', 'automake', 'build-essential', 'git libcurl4-openssl-dev', 'libgeoip-dev', 'liblmdb-dev', 'libpcre++-dev', 'libtool', 'libxml2-dev', 'libyajl-dev', 'pkgconf', 'wget', 'zlib1g-dev']:
+    ensure => installed,
+    }
 }
