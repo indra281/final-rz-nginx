d79e401020d7945f8049fa8acfc7f24e81f70028
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-13 02:23:50 +0530

    adding mod security

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,4 +18,34 @@ class nginx::config {
     cwd  => '/home',
     path => ['/usr/bin', '/usr/sbin',],
   }
+
+  exec {'git submodule init':
+        cwd     => '/home/ModSecurity',
+        command => 'git submodule init' ,
+    }
+
+  exec {'git submodule update':
+        cwd     => '/home/ModSecurity',
+        command => 'git submodule update' ,
+    }
+
+  exec {'run build.sh':
+        cwd     => '/home/ModSecurity',
+        command => './build.sh' ,
+    }
+
+  exec {'run configure':
+        cwd     => '/home/ModSecurity',
+        command => './configure' ,
+    }
+
+  exec {'Make it ':
+        cwd     => '/home/ModSecurity',
+        command => 'make' ,
+    }
+
+  exec {'Make install ':
+        cwd     => '/home/ModSecurity',
+        command => 'make install' ,
+    }
 }
