0096a155422e9de655f14aa70258f672d1a2ef32
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-13 02:26:45 +0530

    asfd

--- manifests/config.pp
+++ manifests/config.pp
@@ -22,30 +22,36 @@ class nginx::config {
   exec {'git submodule init':
         cwd     => '/home/ModSecurity',
         command => 'git submodule init' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'git submodule update':
         cwd     => '/home/ModSecurity',
         command => 'git submodule update' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'run build.sh':
         cwd     => '/home/ModSecurity',
         command => './build.sh' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'run configure':
         cwd     => '/home/ModSecurity',
         command => './configure' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'Make it ':
         cwd     => '/home/ModSecurity',
         command => 'make' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'Make install ':
         cwd     => '/home/ModSecurity',
         command => 'make install' ,
+        path    => ['/usr/bin', '/usr/sbin',],
     }
 }
