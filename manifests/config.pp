1fb40ccf9409fb838f3d56a7cf4b407582ddbb6e
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-14 00:21:27 +0530

    saffas

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,50 +4,57 @@
 class nginx::config {
   exec { 'git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity':
     onlyif => 'test ! -f /home/ModSecurity',
-    cwd    => '/home',
-    path   => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
+    cwd       => '/home',
+    path      => ['/usr/bin', '/usr/sbin',],
   }
 
   exec {'git submodule init':
-        cwd     => '/home/ModSecurity',
-        timeout => 10000,
-        command => 'git submodule init' ,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        timeout   => 10000,
+        command   => 'git submodule init' ,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'git submodule update':
-        cwd     => '/home/ModSecurity',
-        timeout => 10000,
-        command => 'git submodule update' ,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        timeout   => 10000,
+        command   => 'git submodule update' ,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'run build.sh':
-        cwd     => '/home/ModSecurity',
-        command => 'bash build.sh' ,
-        timeout => 10000,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        command   => 'bash build.sh' ,
+        timeout   => 10000,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'run configure':
-        cwd     => '/home/ModSecurity',
-        timeout => 10000,
-        command => 'bash configure' ,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        timeout   => 10000,
+        command   => 'bash configure' ,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'Make it ':
-        cwd     => '/home/ModSecurity',
-        command => 'make',
-        timeout => 10000,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        command   => 'make',
+        timeout   => 10000,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   exec {'Make install ':
-        cwd     => '/home/ModSecurity',
-        timeout => 10000,
-        command => 'make install' ,
-        path    => ['/usr/bin', '/usr/sbin',],
+        cwd       => '/home/ModSecurity',
+        logoutput => true,
+        timeout   => 10000,
+        command   => 'make install' ,
+        path      => ['/usr/bin', '/usr/sbin',],
     }
 
   file {'/etc/nginx/conf.d':
