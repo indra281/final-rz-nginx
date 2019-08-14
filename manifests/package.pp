76ec716c70dea18c92a395e62f0507a42f61f36a
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-14 13:26:59 +0530

    asff

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,7 +2,14 @@
 #This class will install open source nginx
 #
 class nginx::package {
-  package {['https://extras.getpagespeed.com/release-el7-latest.rpm', 'nginx']:
+
+  exec {'Copy objs  ':
+        logoutput => true,
+        command   => 'yum -y install https://extras.getpagespeed.com/release-el7-latest.rpm',
+        path      => ['/usr/bin', '/usr/sbin',],
+    }
+
+  package {['nginx', 'nginx-module-security', 'nginx-owasp-crs']:
     ensure => installed,
   }
 
